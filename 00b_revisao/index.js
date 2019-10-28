ea6fe2e40757d91ef9a35a26bffc0cb73c947c6f
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-10-28 13:36:39 -0300

    reseta exercicios, deixando sem resolução na branch master

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -1,61 +1,14 @@
 // 1.
-// [x] adicionar a tag <script> no html
-// [x] verificar se está lendo index.js
-// [x] alterar cor de <body>
-// [x] escutar o clique do botão
-// [x] criar função de alterar a cor usando rgb()
-// [x] usar Math.random() e Math.floor() ou parseInt()
+// [] adicionar a tag <script> no html
+// [] verificar se está lendo index.js
+// [] alterar cor de <body>
+// [] escutar o clique do botão
+// [] criar função de alterar a cor usando rgb()
+// [] usar Math.random() e Math.floor() ou parseInt()
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [x] remover repetição de código
-// [x] anonymous function
-// [x] arrow function
+// [] remover repetição de código
+// [] anonymous function
+// [] arrow function
 // [] template string
-
-const corpo = document.querySelector("body");
-const botao = document.getElementById("trocaCor");
-
-// Anonymous function
-
-// corpo.style.backgroundColor = "purple";
-// botao.addEventListener("click", function() {
-//     const corRandomicaR = geraNumero();
-//     const corRandomicaG = geraNumero();
-//     const corRandomicaB = geraNumero();
-
-//     corpo.style.backgroundColor = "rgb(" + corRandomicaR + "," + corRandomicaG + "," + corRandomicaB + ")";
-// });
-
-// function geraNumero() {
-//     const numeroInteiro = Math.floor(Math.random() * 255);
-//     return numeroInteiro;
-// }
-
-// Arrow function
-
-// corpo.style.backgroundColor = "purple";
-// botao.addEventListener("click", () => {
-//     const corRandomicaR = geraNumero();
-//     const corRandomicaG = geraNumero();
-//     const corRandomicaB = geraNumero();
-
-//     corpo.style.backgroundColor = "rgb(" + corRandomicaR + "," + corRandomicaG + "," + corRandomicaB + ")";
-// });
-
-// geraNumero = () => {
-//     return Math.floor(Math.random() * 255);;
-// }
-
-// Arrow function sem () para um ÚNICO parâmetro, sem {} e sem return
-
-corpo.style.backgroundColor = "purple";
-botao.addEventListener("click", () => {
-    const corRandomicaR = geraNumero(255);
-    const corRandomicaG = geraNumero(255);
-    const corRandomicaB = geraNumero(255);
-
-    corpo.style.backgroundColor = "rgb(" + corRandomicaR + "," + corRandomicaG + "," + corRandomicaB + ")";
-});
-
-geraNumero = num => Math.floor(Math.random() * num);
