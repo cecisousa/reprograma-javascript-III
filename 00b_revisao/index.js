4ed60f4874947161b3f6e5f7d232ab3310b14762
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-10-29 13:27:44 -0300

    finaliza exercicio ajax/json

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -11,6 +11,7 @@
 // [x] remover repetição de código
 // [x] anonymous function
 // [x] arrow function
+// [x] template string
 
 const corpo = document.querySelector("body");
 const botao = document.getElementById("trocaCor");
@@ -48,13 +49,52 @@ const botao = document.getElementById("trocaCor");
 
 // Arrow function sem () para um ÚNICO parâmetro, sem {} e sem return
 
+// corpo.style.backgroundColor = "purple";
+// botao.addEventListener("click", () => {
+//     const corRandomicaR = geraNumero(255);
+//     const corRandomicaG = geraNumero(255);
+//     const corRandomicaB = geraNumero(255);
+
+//     corpo.style.backgroundColor = "rgb(" + corRandomicaR + "," + corRandomicaG + "," + corRandomicaB + ")";
+// });
+
+// geraNumero = num => Math.floor(Math.random() * num);
+
+// Template String
+
+// corpo.style.backgroundColor = "purple";
+// botao.addEventListener("click", () => {
+//     const corRandomicaR = geraNumero(255);
+//     const corRandomicaG = geraNumero(255);
+//     const corRandomicaB = geraNumero(255);
+
+//     corpo.style.backgroundColor = `rgb(${corRandomicaR}, ${corRandomicaG}, ${corRandomicaB})`;
+// });
+
+// Classes
 corpo.style.backgroundColor = "purple";
-botao.addEventListener("click", () => {
-    const corRandomicaR = geraNumero(255);
-    const corRandomicaG = geraNumero(255);
-    const corRandomicaB = geraNumero(255);
+botao.addEventListener("click", mudaBody);
+class MudaCor {
+    constructor() {
+        this.r = this.geraNumero(255);
+        this.g = this.geraNumero(255);
+        this.b = this.geraNumero(255);
+    };
 
-    corpo.style.backgroundColor = "rgb(" + corRandomicaR + "," + corRandomicaG + "," + corRandomicaB + ")";
-});
+    geraNumero(num) {
+        return Math.floor(Math.random() * num);
+    };
 
-geraNumero = num => Math.floor(Math.random() * num);
+    geraRGB() {
+        return `rgb(${this.r}, ${this.g}, ${this.b})`;
+    }
+};
+
+function mudaBody () {
+    const mudaColor = new MudaCor();
+    corpo.style.backgroundColor = mudaColor.geraRGB();
+
+    // Também podemos instanciar direto:
+    // corpo.style.backgroundColor = new MudaCor().geraRGB();
+}
+
--- 01a_ajax_json/index.js
+++ 01a_ajax_json/index.js
@@ -1,4 +1,40 @@
-// [] instanciar XMLHttpRequest()
-// [] o que fazer depois que receber a resposta
-// [] fazer a requisição com open() e send()
-// [] tratar response
+// [x] instanciar XMLHttpRequest()
+// [x] o que fazer depois que receber a resposta
+// [x] fazer a requisição com open() e send()
+// [x] tratar response
+const div = document.getElementById("root");
+const imagem = document.createElement("img");
+const request = new XMLHttpRequest();
+
+request.onreadystatechange = getDados;
+
+function getDados() {
+    if(request.readyState === 4 && request.status === 200) {
+        const response = request.response;
+        const json = JSON.parse(response);
+        imagem.src = json.data[0].imagem;
+        div.appendChild(imagem);
+    } else {
+        console.log(request.readyState, request.status);
+    }
+}
+
+// forma detalhada da function:
+
+// function getDados() {
+//     if(request.readyState === 4 && request.status === 200) {
+//         const response = request.response;
+//         const json = JSON.parse(response);
+//         const data = json.data;
+//         const obj = data[0];
+//         const imgSrc = obj.imagem;
+//         imagem.src = imgSrc;
+//         div.appendChild(imagem);
+//     } else {
+//         console.log(request.readyState, request.status);
+//     }
+// }
+
+const url = "./data/dados.json";
+request.open("GET", url);
+request.send(null);
