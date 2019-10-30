055ac870870e084b37ece45c18e12a593cf54465
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-10-30 16:38:33 -0300

    cria resoluções utilizando outras opções de loop

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -2,12 +2,9 @@
 // [x] o que fazer depois que receber a resposta
 // [x] fazer a requisição com open() e send()
 // [x] tratar response
-
-
-
 // [x] iterar sobre o array e mostrar as três imagens usando for()
-// [] iterar sobre o array e mostrar as três imagens usando for...of
-// [] iterar sobre o array e mostrar as três imagens usando forEach
+// [x] iterar sobre o array e mostrar as três imagens usando for...of
+// [x] iterar sobre o array e mostrar as três imagens usando forEach
 
 // Versão 1 (sem array):
 
@@ -39,37 +36,84 @@
 // request.open("GET", url);
 // request.send(null);
 
-// Versão 2 (utilizando array):
+// Versão 2 (utilizando for):
 
 const request = new XMLHttpRequest();
 const div = document.getElementById("root");
 request.onreadystatechange = getFotos;
+
+// function getFotos() {
+//     if (request.readyState === 4 && request.status === 200) {
+//         const response = request.response;
+//         const json = JSON.parse(response);
+//         const data = json.data;
+        
+//         for (let i = 0; i < data.length; i++) {
+//             const img = document.createElement("img");
+//             div.appendChild(img);
+//             img.src = data[i].imagem;
+//             const txt  = document.createElement("p");
+//             div.appendChild(txt);
+//             txt.textContent = data[i].nome;
+
+//             // outra forma de criar o for:
+//             // const element = data[i];
+//             // const img = `<img src=${element.imagem}>`;
+//             // div.innerHTML += img
+
+//         }
+//     } else  {
+//         console.log(request.readyState, request.status);
+//     }
+// }
+
+const url = "./data/dados.json"
+request.open("GET", url);
+request.send(null);
+
+// Versão 3 (utilizando forEach):
+
+// function getFotos() {
+//     if (request.readyState === 4 && request.status === 200) {
+//         const response = request.response;
+//         const json = JSON.parse(response);
+//         const data = json.data;
+
+//         data.forEach(element => {
+//             const img = document.createElement("img");
+//             div.appendChild(img);
+//             img.src = element.imagem;
+//             const txt  = document.createElement("p");
+//             div.appendChild(txt);
+//             txt.textContent = element.nome;
+//         });
+        
+//     } else  {
+//         console.log(request.readyState, request.status);
+//     }
+// }
+
+// Versão 4 (utilizando for...of):
 
 function getFotos() {
     if (request.readyState === 4 && request.status === 200) {
         const response = request.response;
         const json = JSON.parse(response);
         const data = json.data;
-        
-        for (let i = 0; i < data.length; i++) {
+
+        for (const posicao of data) {
             const img = document.createElement("img");
             div.appendChild(img);
-            img.src = data[i].imagem;
+            img.src = posicao.imagem;
             const txt  = document.createElement("p");
             div.appendChild(txt);
-            txt.textContent = data[i].nome;
-
-            // outra forma de criar o for:
-            // const element = data[i];
-            // const img = `<img src=${element.imagem}>`;
-            // div.innerHTML += img
-
+            txt.textContent = posicao.nome;
         }
+        
     } else  {
         console.log(request.readyState, request.status);
     }
 }
 
-const url = "./data/dados.json"
-request.open("GET", url);
-request.send(null);
+
+            
