1c50312a609ae1aba3df61742bb16c4b6dc65273
cecisousa <ceciliasousa21@gmail.com>
2019-10-31 10:03:10 -0300

    finaliza exercicio 1f sobre ajax/api e form

--- 01f_ajax_giphy_search_input/index.js
+++ 01f_ajax_giphy_search_input/index.js
@@ -4,6 +4,28 @@
 
 const request = new XMLHttpRequest();
 const input = document.getElementById("gifInput");
-const busca = input.value;
+const body = document.body;
+const btn = document.getElementById("gifButton");
 
-console.log(busca);
+btn.addEventListener("click", function(evento){
+    request.onreadystatechange = pesquisaGif;
+    evento.preventDefault();
+    const textoBuscado = input.value;
+    const chaveAPI = "dc6zaTOxFJmzC";
+    const url = `http://api.giphy.com/v1/gifs/search?q=${textoBuscado}&api_key=${chaveAPI}&limit=3&offset=0`
+    request.open("GET", url);
+    request.send(null);
+
+    function pesquisaGif () {
+        if (request.readyState === 4 && request.status === 200) {
+            const response = request.response;
+            const json = JSON.parse(response);
+            const img = document.createElement("img");
+            body.appendChild(img);
+            img.src = json.data[1].images.original.url;
+            console.log(img.src)
+        } else {
+            console.log(request.readyState, request.status);
+        }
+    }  
+});
